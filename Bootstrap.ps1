2610c28feb947e902ec42a3ccae1a5390e830a36
Joakim Skoog <joakimskooog@gmail.com>
2024-08-30 14:09:30 +0200

    Update Bootstrap.ps1

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -10,12 +10,8 @@ New-Item $DownloadFolder -ItemType directory;
 
 Invoke-WebRequest -Uri $GithubUrl -OutFile $DownloadedFile;
 
-
-#Add-Type -AssemblyName System.IO.Compression.FileSystem;
-#[System.IO.Compression.ZipFile]::ExtractToDirectory($DownloadedFile, $DownloadFolder);
-
-Write-Host $DotfilesFolder;
-#$SetupPath = (Join-Path -Path $DownloadFolder -ChildPath "dotfiles-main" | Join-Path -ChildPath "Setup.ps1");
+Add-Type -AssemblyName System.IO.Compression.FileSystem;
+[System.IO.Compression.ZipFile]::ExtractToDirectory($DownloadedFile, $DownloadFolder);
 
 Push-Location $DotfilesFolder;
 Invoke-Expression (Join-Path $DotfilesFolder "Setup.ps1")
