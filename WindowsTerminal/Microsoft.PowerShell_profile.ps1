64de117a79f428198b02786e59357207140ca74a
joakimskoog <joakimskooog@gmail.com>
2024-09-04 11:18:48 +0200

    Make the predictions use history and show the suggestions in a listview

--- WindowsTerminal/Microsoft.PowerShell_profile.ps1
+++ WindowsTerminal/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,15 @@ Import-Module "PSReadLine";
 
 oh-my-posh --init --shell pwsh --config ~/.oh-my-posh-custom-theme.omp.json | Invoke-Expression;
 
+
+########## PSReadLine ##########
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
 ########## Git Aliases ##########
 function Git-Status {
     git status -s;
