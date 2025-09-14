89d5e0fcbaee81a9f56baae457c968a0a49b0fde
joakimskoog <joakimskooog@gmail.com>
2025-09-14 14:51:03 +0200

    Starting minimalist dotfiles setup

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -80,25 +80,31 @@ clear
 Splash
 
 echo "`nStarting bootstrap..."
-# Install Git for windows because Git is not preinstalled on Windows
-winget install --id Git.Git -e --source winget --accept-package-agreements --accept-source-agreements --silent --no-upgrade 1> $null
 
-# Refresh the PATH so we can use Git in the current session
-$env:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
+if(-not (Get-Command git -ErrorAction SilentlyContinue)) {
+    echo "Installing git..."
+    # Install Git for windows because Git is not preinstalled on Windows
+    winget install --id Git.Git -e --source winget --accept-package-agreements --accept-source-agreements --silent --no-upgrade 1> $null
+
+    # Refresh the PATH so we can use Git in the current session
+    $env:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
+}
 
 $repo = "https://github.com/joakimskoog/.dotfiles.git"
 $repoBranch = "rework"
 $repoFolder = "$HOME/.dotfiles"
+if (-not (Test-Path -Path $repoFolder -PathType Container)) {
+    echo "Cloning from $repo into $repoFolder"
+    git clone $repo $repoFolder *> $null
 
-echo "Cloning from $repo into $repoFolder"
-git clone $repo $repoFolder *> $null
-
-if ($repoBranch -ne "main") {
-    Push-Location $repoFolder
-    git fetch origin $repoBranch *> $null
-    git checkout $repoBranch *> $null
-    Pop-Location
+    if ($repoBranch -ne "main") {
+        Push-Location $repoFolder
+        git fetch origin $repoBranch *> $null
+        git checkout $repoBranch *> $null
+        Pop-Location
+    }
 }
 
-echo "Installing..."
+echo "Configuring dotfiles..."
 . $HOME/.dotfiles/install.ps1
+echo "Configuration done!"
--- install.ps1
+++ install.ps1
@@ -1,14 +1,22 @@
+function Symlink {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Target,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Link
+    )
+
+    New-Item -Path $Link -ItemType SymbolicLink -Value $Target -Force > $null
+}
+
 # Stop execution on any error
 $ErrorActionPreference = "Stop"
 
 $path = "$HOME/.dotfiles"
 $pathInstall = "$path/install"
 
-# Packages
-
-
-# Config
-. $pathInstall/config/git.ps1
-
-
-# Finishing up
+# Git
+Symlink "$path/git/.gitconfig" "$HOME/.gitconfig"
+git config --global user.name  "$(Read-Host '[git] user.name')"
+git config --global user.email "$(Read-Host '[git] user.email')"
